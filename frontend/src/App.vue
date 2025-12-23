4f61e8c51654d4073d462fa9a509cb5bbe70e098
Sam Yendell <sam.yendell@ajbell.co.uk>
2025-12-23 11:06:06 +0000

    Initialize frontend with Vue 3 and Vite: add basic structure, routing, and favicon; remove outdated server files

---
+++ frontend/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/login">Login</router-link>
+    </nav>
+    <router-view />
+    
+    <h1>{{ welcome_message }}</h1>
+    <p>counter: {{ counter }}</p>
+    <button v-on:click="increment">Add one</button>
+    
+    <p v-if="counter >= 5">Big number</p>
+    <p v-else>Little number</p>
+
+    <Login />
+
+  </div>
+</template>
+
+<script>
+  import Login from "./Login.vue"
+  
+  export default {
+    data(){
+      return {
+        welcome_message: "Hello World!"
+      }
+    },
+    methods: {
+      increment(){
+        this.counter ++;
+      }
+    },
+    components: {
+      Login
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <hi>Home page</hi>
+    </div>
+</template>
